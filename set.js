101042c799f85d90fdf9225a7512bcb14d671fc5
Abhirajrs1 <132868953+Abhirajrs1@users.noreply.github.com>
2024-10-24 16:54:29 +0530

    Modifications in set DS

--- set.js
+++ set.js
@@ -1,10 +1,22 @@
 const arr=[1,2,3,4,5,1,2]
 const a=new Set(arr)
-// a.add(6)
-// for(let item of a){
-//     console.log(item);
-// }
+a.add(6)               //Add new value to the set
+console.log(a.has(6)); //Check for that the value exist in the set
+console.log(a.size);   //Check the size of the set
+
+a.delete(2)            //Delete a value from the set
+a.clear()              //Delete or clear all the elements
+
+ 
+for(let item of a){
+    console.log(item);
+}
 console.log(a);
-console.log([...a]);
+
+
+// Convert the set into an array
+
+console.log(Array.from(a))  
+console.log([...a]); 
 
 
