579fc326f39cd52cf7cf309bf2b7138039f3fd15
Abhirajrs1 <132868953+Abhirajrs1@users.noreply.github.com>
2025-01-26 21:30:06 +0530

    More method added

--- Map/map.js
+++ Map/map.js
@@ -16,4 +16,7 @@ map.forEach((value,key )=> {
 
 console.log(map.get('name')) //Retrieve the value from map using key
 console.log((map.has('name')));  //Check the key existing in the map
+console.log(map.size); //Check the size of the map
+map.clear() //Delete all in the map
+
 
